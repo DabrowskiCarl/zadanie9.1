074b9543a138f7e6c319a74984cb574f9e04af33
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-03-20 23:54:42 +0100

    Add files, folders, scripts, all

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function getTriangleArea(a, h) {
+    if (a <= 0 || h <= 0) {
+        return "Wrong input data";
+    } else if (a > 0 && h > 0) {
+        return a * h / 2;
+    }
+}
+const firstTriangle = getTriangleArea(5, 2);
+console.log(firstTriangle);
+const secondTriangle = getTriangleArea(-1, 3);
+console.log(secondTriangle);
+const thirdTriangle = getTriangleArea(2, 2);
+console.log(thirdTriangle);
